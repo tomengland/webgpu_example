586e2ddd7fb5e36ffd573f79ee74d24740edcc91
Thomas England <tenglan4@jh.edu>
2025-06-15 19:00:13 -1000

    get adapter!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include <webgpu/webgpu.h>
+#include <cassert>
 #include <iostream>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif // __EMSCRIPTEN__
 
+void onAdapterReqeustEnded(WGPURequestAdapterStatus status, WGPUAdapter adapter, char const* message, void* userdata)
+{
+    bool* pRequestEnded = static_cast<bool*>(userdata);
+    *pRequestEnded = true;
+}
+WGPUAdapter requestAdapterSync(WGPUInstance instance, WGPURequestAdapterOptions const * options) {
+    // A simple structure holding the local information shared with the
+    // onAdapterRequestEnded callback.
+    struct UserData {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded = false;
+    };
+    UserData userData;
+
+    // Callback called by wgpuInstanceRequestAdapter when the request returns
+    // This is a C++ lambda function, but could be any function defined in the
+    // global scope. It must be non-capturing (the brackets [] are empty) so
+    // that it behaves like a regular C function pointer, which is what
+    // wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
+    // is to convey what we want to capture through the pUserData pointer,
+    // provided as the last argument of wgpuInstanceRequestAdapter and received
+    // by the callback as its last argument.
+    auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const * message, void * pUserData) {
+        UserData& userData = *reinterpret_cast<UserData*>(pUserData);
+        if (status == WGPURequestAdapterStatus_Success) {
+            userData.adapter = adapter;
+        } else {
+            std::cout << "Could not get WebGPU adapter: " << message << std::endl;
+        }
+        userData.requestEnded = true;
+    };
+
+    // Call to the WebGPU request adapter procedure
+    wgpuInstanceRequestAdapter(
+        instance /* equivalent of navigator.gpu */,
+        options,
+        onAdapterRequestEnded,
+        (void*)&userData
+    );
+
+    // We wait until userData.requestEnded gets true
+    // [...] Wait for request to end
+#ifdef __EMSCRIPTEN__
+    while (!userData.requestEnded) {
+        emscripten_sleep(100);
+    }
+#endif // __EMSCRIPTEN__
+    assert(userData.requestEnded);
+
+    return userData.adapter;
+}
 int main() {
     // create descriptor
     WGPUInstanceDescriptor desc = {};
@@ -19,7 +74,22 @@ int main() {
     }
 
     std::cout << "WGPU instance: " << instance << std::endl;
-    // Clean up the instance
+    std::cout << "Requesting adapter..." << std::endl;
+
+    WGPURequestAdapterOptions requestAdapterOptions = {};
+    requestAdapterOptions.nextInChain = nullptr;
+    WGPUAdapter adapter = requestAdapterSync(instance, &requestAdapterOptions);
+    if (!adapter)
+    {
+        std::cerr << "Failed to get WGPU adapter." << std::endl;
+        wgpuInstanceRelease(instance);
+        return 1;
+    }
     wgpuInstanceRelease(instance);
+    std::cout << "WGPU adapter: " << adapter << std::endl;
+
+
+    // Clean up the instance
+    wgpuAdapterRelease(adapter);
     return 0;
 }
